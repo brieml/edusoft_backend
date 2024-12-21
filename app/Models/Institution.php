07f79a70047206845af335364dc633c1ea25c6eb
DarwinMontes <37778642+mdarwin1992@users.noreply.github.com>
2024-12-20 19:50:31 -0500

    feat(institutions): agregar modelo de institutions

--- app/Models/Institution.php
+++ app/Models/Institution.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Institution extends Model
 {
     use HasFactory;
+
+    protected $table = 'institutions';
+    protected $fillable = [
+        'dane_code', 'name', 'nit', 'principal_name', 'principal_id',
+        'sector', 'calendar', 'entity_type', 'department_id', 'city_id',
+        'address', 'rural_area', 'zone', 'phones', 'website', 'email'
+    ];
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function specialty(): BelongsTo
+    {
+        return $this->belongsTo(Specialty::class);
+    }
 }
